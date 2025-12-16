91d9bf900b12800eedc5e9a6106265b8bc747990
Mael7w7 <ange_lito2002@hotmail.com>
2025-12-16 03:03:17 -0500

    primer commit

---
+++ src/main/java/com/angelHc/Gestion/de/Citas/Medicas/GestionDeCitasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.angelHc.Gestion.de.Citas.Medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeCitasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeCitasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelHc/Gestion/de/Citas/Medicas/GestionDeCitasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelHc.Gestion.de.Citas.Medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeCitasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
